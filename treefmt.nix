33673d0647697984cd6e7ed6536b5e9537dffa78
Thomas Arcila <134677+tarcila@users.noreply.github.com>
2025-03-19 16:11:53 -0400

    Treefmt configuration update

--- treefmt.nix
+++ treefmt.nix
@@ -2,12 +2,14 @@
 {
   # Used to find the project root
   projectRootFile = "flake.nix";
+  programs.black.enable = true;
   programs.cmake-format.enable = true;
   programs.jsonfmt = {
     enable = true;
     package = pkgs.hujsonfmt;
   };
   programs.just.enable = true;
+  programs.mdformat.enable = true;
   programs.nixfmt.enable = true;
   programs.shfmt.enable = true;
   programs.yamlfmt.enable = true;
